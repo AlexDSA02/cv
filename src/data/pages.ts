69559917eb92ce70417fff2299590d8774b1d8f7
AlexDSA02 <alexandre.dsa02@gmail.com>
2025-02-19 06:06:00 +0100

    ajust img path

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -1,6 +1,6 @@
 export const pages = [
-    {namePage: "Degrees", pathImage: "../assets/degrees.jpeg", pathRouterLink: "Degrees", pathPage: "/degrees"},
-    {namePage: "Experiences", pathImage: "../assets/experiences.jpeg", pathRouterLink: "Experiences", pathPage: "/experiences"},
-    {namePage: "Skills", pathImage: "../assets/skills.jpeg", pathRouterLink: "Skills", pathPage: "/skills"},
-    {namePage: "About", pathImage: "../assets/aboutMe.jpeg", pathRouterLink: "About", pathPage: "/about"},
+    {namePage: "Degrees", pathImage: "../../public/degrees.jpeg", pathRouterLink: "Degrees", pathPage: "/degrees"},
+    {namePage: "Experiences", pathImage: "../../public/experiences.jpeg", pathRouterLink: "Experiences", pathPage: "/experiences"},
+    {namePage: "Skills", pathImage: "../../public/skills.jpeg", pathRouterLink: "Skills", pathPage: "/skills"},
+    {namePage: "About", pathImage: "../../public/aboutMe.jpeg", pathRouterLink: "About", pathPage: "/about"},
 ];
